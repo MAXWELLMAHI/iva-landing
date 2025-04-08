82b70d2a51bb4947ee2477e91403de7e5b92d6fa
Mahesh golam <maheshgolam12@gmail.com>
2025-04-08 06:41:55 +0530

    Remove donation references and update project name

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -97,9 +97,6 @@ const Hero = () => {
                   {['mpact.', 'ision.', 'pproach.'][index]}
                 </motion.div>
               ))}
-              <motion.div className="hero__nav-item" variants={itemVariants}>
-                <a href="/donations" className="hero__nav-link">Donate</a>
-              </motion.div>
             </motion.nav>
           </div>
         </motion.div>
